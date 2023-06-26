c0ce39c80c57cc7c8ddde50963b25cadaae33f6f
Trung4123 <thieuvinhtrung4123@gmail.com>
2023-06-26 12:37:22 +0700

    update css and interface

--- js/main.js
+++ js/main.js
@@ -504,3 +504,27 @@ $('.close_iframe, .class_blur').click(function () {
 	$('body').removeClass('hidden');
 }
 );
+
+// cursor
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) => {
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
+document.querySelectorAll('a').forEach(links => {
+    links.onmouseenter = () => {
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () => {
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+
+});
