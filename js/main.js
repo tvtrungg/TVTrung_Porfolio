41acfdf8deca2fb01dae731dc9c4c1eb0b7c5a69
Trung4123 <thieuvinhtrung4123@gmail.com>
2023-07-25 23:47:02 +0700

    remove cursor animate

--- js/main.js
+++ js/main.js
@@ -507,27 +507,27 @@ $('.close_iframe, .class_blur').click(function () {
 
 
 // Cursor
-let cursor1 = document.querySelector('.cursor-1');
-let cursor2 = document.querySelector('.cursor-2');
+// let cursor1 = document.querySelector('.cursor-1');
+// let cursor2 = document.querySelector('.cursor-2');
 
-window.onmousemove = (e) => {
-	cursor1.style.top = e.pageY + 'px';
-	cursor1.style.left = e.pageX + 'px';
-	cursor2.style.top = e.pageY + 'px';
-	cursor2.style.left = e.pageX + 'px';
-}
+// window.onmousemove = (e) => {
+// 	cursor1.style.top = e.pageY + 'px';
+// 	cursor1.style.left = e.pageX + 'px';
+// 	cursor2.style.top = e.pageY + 'px';
+// 	cursor2.style.left = e.pageX + 'px';
+// }
 
-document.querySelectorAll('a').forEach(links => {
-	links.onmouseenter = () => {
-		cursor1.classList.add('active');
-		cursor2.classList.add('active');
-	}
+// document.querySelectorAll('a').forEach(links => {
+// 	links.onmouseenter = () => {
+// 		cursor1.classList.add('active');
+// 		cursor2.classList.add('active');
+// 	}
 
-	links.onmouseleave = () => {
-		cursor1.classList.remove('active');
-		cursor2.classList.remove('active');
-	}
-});
+// 	links.onmouseleave = () => {
+// 		cursor1.classList.remove('active');
+// 		cursor2.classList.remove('active');
+// 	}
+// });
 
 
 // hide menu when click on menu item
